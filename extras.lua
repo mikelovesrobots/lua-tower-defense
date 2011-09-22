150d7ddc0cbe30e54d5e4a21a022f57a6678fdfc
Mike Judge <mikelovesrobots@gmail.com>
2011-09-21 22:31:56 -0700

    Added my extras file

---
+++ extras.lua
@@ -0,0 +1,22 @@
+function debug(string)
+  if (DEBUG) then
+    print(string)
+  end
+end
+
+function between(val, min, max)
+  return val >= min and val <= max
+end
+
+function math.dist(x1, y1, x2, y2)
+  return ((x2-x1)^2+(y2-y1)^2)^0.5
+end
+
+function set_color(rgb)
+  love.graphics.setColor(rgb[1], rgb[2], rgb[3])
+end
+
+function round(num)
+  if num >= 0 then return math.floor(num+.5)
+  else return math.ceil(num-.5) end
+end
